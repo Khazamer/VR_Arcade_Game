4aea69844798cc6d477494ead55cd35f854170a3
Nathan Khazam <nakh6169@colorado.edu>
2024-11-07 11:32:46 -0800

    Gun works and now doing splines and enemies

--- Assets/Scripts/smgShoot.cs
+++ Assets/Scripts/smgShoot.cs
@@ -14,6 +14,8 @@ public class smgShoot : MonoBehaviour
     private float SMGTimer = 0.05f;
     public InputActionReference rightTrigger;
     public InputActionReference leftTrigger;
+    public ParticleSystem muzzleFlash;
+    public AudioClip gunShot;
 
     void Update() {
         currTimer += Time.deltaTime;
@@ -22,13 +24,19 @@ public class smgShoot : MonoBehaviour
             if ((gameObject.transform.parent.name == "Right hand" && rightTrigger.action.ReadValue<float>() > 0.5) || (gameObject.transform.parent.name == "Left hand" && leftTrigger.action.ReadValue<float>() > 0.5)) {
                 if (SMGTimer - currTimer <= 0) {
                     //Debug.Log("SMG shot");
-                    GameObject newSMGBullet = Instantiate(SMGBulletTemplate, transform.parent.position + (transform.parent.forward * 1f), transform.parent.rotation);
+                    GameObject newSMGBullet = Instantiate(SMGBulletTemplate, transform.position + (transform.forward * 0.2f) + (transform.up * 0.1f), transform.rotation);
                     newSMGBullet.GetComponent<Rigidbody>().AddForce(transform.forward * shootPower);
                     Destroy(newSMGBullet, 2);
 
-                    GameObject newSMGShell = Instantiate(SMGBulletTemplate, transform.parent.position + (transform.parent.right * 0.5f), transform.parent.rotation);
-                    //newSMGShell.GetComponent<Rigidbody>().AddForce(transform.forward * shootPower);
+                    GameObject newSMGShell = Instantiate(SMGBulletTemplate, transform.position + (transform.forward * -0.05f) + (transform.right * 0.04f) + (transform.up * 0.1f), transform.rotation);
+                    newSMGShell.GetComponent<Rigidbody>().AddForce(transform.right * (shootPower / 10f));
+                    newSMGShell.GetComponent<Rigidbody>().AddForce(new Vector3(0, -1 * (shootPower / 10f), 0));
                     Destroy(newSMGShell, 1);
+
+                    muzzleFlash.Play();
+
+                    GetComponent<AudioSource>().PlayOneShot(gunShot);
+                    //GetComponent<AudioSource>().Play(0);
 
                     currTimer = 0f;
                 }
